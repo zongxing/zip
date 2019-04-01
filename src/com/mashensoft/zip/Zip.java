2291dd70e86da0827f598ef77ea25f8ef43cc9b6
zongxing <zongxing@vip.qq.com>
2019-04-01 17:40:42 +0800

    核心类增加了注释，并精简了代码

--- src/com/mashensoft/zip/Zip.java
+++ src/com/mashensoft/zip/Zip.java
@@ -20,12 +20,14 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 	/**
 	 * 压缩到当前文件夹
+	 * 
 	 * @param sourceFile
 	 * @throws Exception
 	 */
 	public static void zipFile(String sourceFile) throws Exception {
-		zipFile(sourceFile,sourceFile);
+		zipFile(sourceFile, sourceFile);
 	}
+
 	/**
 	 * 压缩一个文件
 	 * 
@@ -37,16 +39,14 @@ public class Zip {
 		File file = new File(sourceFile);
 		String fileName = file.getName();
 		System.out.println(fileName);
-		if(fileName.startsWith(".")) {
-			descFilePath = sourceFile+".zip";
+		if (fileName.startsWith(".")) {
+			descFilePath = sourceFile + ".zip";
 			System.out.println(descFilePath);
-		}else if(!fileName.startsWith(".")&&fileName.contains(".")) {
-			descFilePath = sourceFile.substring(0,descFilePath.lastIndexOf("."))+".zip";
+		} else if (!fileName.startsWith(".") && fileName.contains(".")) {
+			descFilePath = sourceFile.substring(0, descFilePath.lastIndexOf(".")) + ".zip";
 			System.out.println(descFilePath);
 		}
-		
-		
-		
+
 		ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(descFilePath));
 		ZipEntry zipEntry = new ZipEntry(fileName);
 		zip.putNextEntry(zipEntry);
@@ -62,99 +62,99 @@ public class Zip {
 		zip.flush();
 		zip.closeEntry();
 		zip.close();
-		
+
 	}
+
 	/**
 	 * 压缩只有一层文件夹的所有文件
+	 * 
 	 * @param sourceFile
 	 * @param descFilePath
 	 * @throws Exception
 	 */
 	public static void zipFileFolderOneLevel(String sourceFile, String descFilePath) throws Exception {
 		File file = new File(sourceFile);
-		if(!file.isDirectory()) {
+		if (!file.isDirectory()) {
 			throw new Exception("不是文件夹");
 		}
 		FileInputStream is = null;
 		String folderName = file.getName();
 		ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(descFilePath));
-		
+
 		File[] files = file.listFiles();
-		for(File f:files) {
-			if(f.isDirectory()) {
+		for (File f : files) {
+			if (f.isDirectory()) {
 				continue;
 			}
-			ZipEntry zipEntry = new ZipEntry(folderName+File.separator+f.getName());
+			ZipEntry zipEntry = new ZipEntry(folderName + File.separator + f.getName());
 			zip.putNextEntry(zipEntry);
-				is = new FileInputStream(f);
-				byte[] b = new byte[1024 * 1024];
-				int len = 0;
-				while ((len = is.read(b)) != -1) {
-					zip.write(b, 0, len);
-				}
-				is.close();
+			is = new FileInputStream(f);
+			byte[] b = new byte[1024 * 1024];
+			int len = 0;
+			while ((len = is.read(b)) != -1) {
+				zip.write(b, 0, len);
+			}
+			is.close();
 		}
-		
+
 		zip.flush();
 		zip.closeEntry();
 		zip.close();
-		
+
 	}
+
 	/**
 	 * 压缩一个文件
-	 * @throws FoundException 
+	 * 
+	 * @throws FoundException
 	 */
-	public static void compressFile(OutputStream zip,File file) throws Exception {
+	public static void compressFile(OutputStream zip, File file) throws Exception {
 		BufferedInputStream is = new BufferedInputStream(new FileInputStream(file));
-		byte[] b = new byte[1024 * 1024*1024];
+		byte[] b = new byte[1024 * 1024 * 1024];
 		int len = 0;
 		while ((len = is.read(b)) != -1) {
 			zip.write(b, 0, len);
 		}
 		is.close();
 	}
-	
+
 	/**
 	 * 递归文件
+	 * 
 	 * @param filePath
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void recursion(String sourceFile, String descFilePath) throws Exception {
-		
-		
+
 		File file = new File(sourceFile);
-		if(!file.isDirectory()) {
+		if (!file.isDirectory()) {
 			throw new Exception("不是文件夹");
 		}
 		FileInputStream is = null;
 		String folderName = file.getName();
-		ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(descFilePath+".zip"));
+		ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(descFilePath + ".zip"));
 		BufferedOutputStream os = new BufferedOutputStream(zip);
-		Files.walkFileTree(Paths.get(sourceFile),new FileVisitor<Object>() {
+		Files.walkFileTree(Paths.get(sourceFile), new FileVisitor<Object>() {
 
 			@Override
 			public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
-				// TODO Auto-generated method stub
 				return FileVisitResult.CONTINUE;
 			}
 
 			@Override
 			public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
-				Path p = (Path)file;
-				//String filePath = p.toString().replace(p.getRoot().toString(), "");
-				String filePath = getFileName(sourceFile,p.toString());
+				Path p = (Path) file;
+				// String filePath = p.toString().replace(p.getRoot().toString(), "");
+				String filePath = getFileName(sourceFile, p.toString());
 				ZipEntry zipEntry = new ZipEntry(filePath);
 				zip.putNextEntry(zipEntry);
 				try {
 					System.out.println(p);
-					compressFile(os,p.toFile());
+					compressFile(os, p.toFile());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				zip.flush();
-//				System.out.println(p.toString());
-//				System.out.println(p.getFileName().toString());
-//				System.out.println(p.getRoot().toString());
 				return FileVisitResult.CONTINUE;
 			}
 
@@ -175,55 +175,27 @@ public class Zip {
 		zip.closeEntry();
 		zip.close();
 	}
+
 	/**
-	 * 递归文件
+	 * 递归遍历解压文件
+	 * 
 	 * @param filePath
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void recursion(String filePath) throws Exception {
 		recursion(filePath, filePath);
-//		
-//		
-//		
-//		
-//		Files.walkFileTree(Paths.get(filePath),new FileVisitor<Object>() {
-//			
-//			@Override
-//			public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
-//				// TODO Auto-generated method stub
-//				return FileVisitResult.CONTINUE;
-//			}
-//			
-//			@Override
-//			public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
-//				//、、File f = (File)file;
-//				Path p = (Path)file;
-//				System.out.println(p.toString());
-//				System.out.println(p.getFileName().toString());
-//				System.out.println(p.getRoot().toString());
-//				return FileVisitResult.CONTINUE;
-//			}
-//			
-//			@Override
-//			public FileVisitResult visitFileFailed(Object file, IOException exc) throws IOException {
-//				System.out.println("失败");
-//				return FileVisitResult.CONTINUE;
-//			}
-//			
-//			@Override
-//			public FileVisitResult postVisitDirectory(Object dir, IOException exc) throws IOException {
-//				// TODO Auto-generated method stub
-//				return FileVisitResult.CONTINUE;
-//			}
-//			
-//		});
-		
+
 	}
-	public static String getFileName(String currentFileName,String fullPath) {
+
+	/**
+	 * 获取解压的文件在磁盘中的文件名，以便于创建File对象
+	 * 
+	 * @param currentFileName 压缩包的文件全路径
+	 * @param fullPath
+	 * @return
+	 */
+	public static String getFileName(String currentFileName, String fullPath) {
 		File file = new File(currentFileName);
-		return file.getName().replace(File.separator, "")+fullPath.replace(currentFileName, "");
-	}
-	public static void main(String[] args) {
-		System.out.println(getFileName("d:/soft","d:/soft/m/b/a.txt"));
+		return file.getName().replace(File.separator, "") + fullPath.replace(currentFileName, "");
 	}
 }
--- test/com/mashensoft/zip/Unzip.java
+++ test/com/mashensoft/zip/Unzip.java
@@ -12,7 +12,12 @@ import java.util.zip.ZipInputStream;
 import org.junit.jupiter.api.condition.OS;
 
 public class Unzip {
-
+	/**
+	 * 解压文件
+	 * @param sourcePath  压缩包文件
+	 * @param descPath	解压到到的目录
+	 * @throws Exception
+	 */
 	public static void unzipFolder(String sourcePath, String descPath) throws Exception {
 		File file = new File(sourcePath);
 		String basePath = file.getParent();
@@ -41,39 +46,34 @@ public class Unzip {
 		while ((entry = zip.getNextEntry()) != null) {
 			String fileName = entry.getName();
 			System.out.println(fileName);
-			
-//			//父目录如果没有文件夹，就创建文件夹
-//			if (entry.isDirectory()) {
-//				// 如果是路径
-//				File f = new File(basePath + File.separator + fileName);
-//				f.mkdirs();
-//			} else {
-			createFolder(sourcePath,fileName);
-				OutputStream os = new BufferedOutputStream(new FileOutputStream(basePath + File.separator + fileName));
-				BufferedInputStream br = new BufferedInputStream(zip);
-				int len = 0;
-				byte[] b = new byte[1028 * 1024];
-				while ((len = br.read(b)) != -1) {
-					os.write(b, 0, len);
-				}
-				os.close();
+			createFolder(sourcePath, fileName);
+			OutputStream os = new BufferedOutputStream(new FileOutputStream(basePath + File.separator + fileName));
+			BufferedInputStream br = new BufferedInputStream(zip);
+			int len = 0;
+			byte[] b = new byte[1028 * 1024];
+			while ((len = br.read(b)) != -1) {
+				os.write(b, 0, len);
 			}
-//		}
+			os.close();
+		}
 		zip.closeEntry();
 		zip.close();
 	}
-	public static String  createFolder(String sourcePath,String filePath) {
+
+	/**
+	 * 创建文件夹，解压时，如果压缩包中的文件在第二层或者以后，则先在本地文件系统中创建文件夹，再解压
+	 * 
+	 * @param sourcePath 压缩包在本地磁盘中的路径
+	 * @param filePath   压缩包里的文件路径
+	 * @return
+	 */
+	public static String createFolder(String sourcePath, String filePath) {
 		String baseFolder = new File(sourcePath).getParent();
 		File file = new File(filePath);
 		String parentFolder = file.getParent();
-		String str = baseFolder+File.separator+parentFolder;
+		String str = baseFolder + File.separator + parentFolder;
 		File desc = new File(str);
 		desc.mkdirs();
 		return str;
-	}
-	public static void main(String[] args) {
-		File file = new File("d:/a.txt");
-		String basePath = file.getParent();
-		System.out.println(basePath);
 	}
 }
